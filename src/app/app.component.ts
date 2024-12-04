b9c3b3bbb0be1c7228a2d4cfddc18896e580bbad
agomcam <agomcam813@iescarrillo.es>
2024-12-04 17:16:10 +0100

    Persona ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PersonListComponent} from './components/person-list/person-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/models/Person.models.ts
@@ -0,0 +1,14 @@
+export class Person {
+  id: number;
+  name: string;
+  surname: string;
+  dni: string;
+
+  constructor(id: number, name: string, surname: string, dni: string) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.dni = dni;
+  }
+
+}
